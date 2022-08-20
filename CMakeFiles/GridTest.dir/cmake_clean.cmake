8f7e1bac5575ee841d95e43849f0359d4fe7a8b4
LadislavHric <ladislavhric95@gmail.com>
2022-08-20 19:55:03 +0200

    makefile replaced by CMakeLists.txt

---
+++ CMakeFiles/GridTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GridTest.dir/grid_test.cpp.o"
+  "CMakeFiles/GridTest.dir/grid_test.cpp.o.d"
+  "GridTest"
+  "GridTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GridTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- makefile
+++
@@ -1,2 +0,0 @@
-default:
-	g++ world/grid.cpp grid_test.cpp -o ./build/test -lsfml-graphics -lsfml-window -lsfml-system
