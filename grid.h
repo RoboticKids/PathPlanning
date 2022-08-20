3d4209bc40b65b4b36d461305ec0b1a95e887b87
LadislavHric <ladislavhric95@gmail.com>
2022-08-20 16:40:28 +0200

    removed redundant class

--- grid.h
+++ grid.h
@@ -5,39 +5,6 @@
 
 // TODO: set formatter
 
-class gridRectangleCell
-{
-    public:
-
-        gridRectangleCell(const size_t id, const float height, const float width):
-        _shape(sf::Vector2f(width, height)), _id(id), _width(width), _height(height){}
-
-        gridRectangleCell(const size_t id, const float x, const float y, const float height, const float width, const sf::Color color):
-        _shape(sf::Vector2f(width, height)), _id(id), _width(width), _height(height)
-        {
-            _x_position = x;
-            _y_position = y;
-            _color = color;
-            _shape.setFillColor(_color);
-            _shape.setPosition(_x_position, _y_position);
-        }
-
-        void setFillCollor(const sf::Color color){_color = color;}
-        void setPosition(const float x, const float y){_x_position = x; _y_position = y;}
-
-        ~gridRectangleCell() = default;
-    
-    private:
-
-        const size_t _id{0};
-        const float _height{0};
-        const float _width {0};
-        float _x_position{0};
-        float _y_position{0};
-        sf::RectangleShape _shape;
-        sf::Color _color;
-};
-
 class GridCreator
 {
     public:
